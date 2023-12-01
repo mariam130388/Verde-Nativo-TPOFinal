340850bddb0d99ccf0cc7f1c25afada7f5355686
mariam130388 <mariam.tomas88@gmail.com>
2023-12-01 15:25:07 -0300

    se agregar usuario admin

--- controladores/usuario_controlador.py
+++ controladores/usuario_controlador.py
@@ -6,14 +6,30 @@ from modelos.usuario_modelo import *
 @app.route('/usuarios', methods=['GET'])
 def obtener_usuarios():
     usuarios = usuarios.query.all()
-    resultado = [{'id': usuario.id, 'nombre': usuario.nombre, 'email': usuario.email} for usuario in usuarios]
+    resultado = [{'id': usuario.id, 'usuario': usuario.usuario, 'contraseña': usuario.contraseña, 'es_administrador':usuario.es_administrador} for usuario in usuarios]
     return jsonify(resultado), 200
 
 # Endpoint para agregar un nuevo usuario
 @app.route('/usuarios', methods=['POST'])
 def agregar_usuario():
     data = request.json
-    nuevo_usuario = Usuario(nombre=data['nombre'], email=data['email'])
+    # es_administrador = data.get('es_administrador', False) #verifica si es adm
+    nuevo_usuario = Usuario(usuario=data['usuario'], contraseña=data['contraseña'])
+    
+    # if es_administrador:
+    #     nuevo_usuario.rol = 'administrador'  # Asigna el rol de administrador al usuario
+        
     db.session.add(nuevo_usuario)
     db.session.commit()
     return jsonify({'mensaje': 'Usuario agregado correctamente'}),201
+
+
+
+
+
+
+
+
+
+
+
--- modelos/usuario_modelo.py
+++ modelos/usuario_modelo.py
@@ -2,9 +2,11 @@ from app import db
 
 class Usuario(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.String(100))
-    email = db.Column(db.String(100))
+    usuario = db.Column(db.String(100))
+    contraseña = db.Column(db.Integer(100))
+    es_administrador= db.Column(db.Integer(1))
 
-    def _init_(self, nombre, email):
-        self.nombre = nombre
-        self.email=email
+    def _init_(self, usuario, contraseña, es_administrador):
+        self.usuario = usuario
+        self.contraseña=contraseña
+        self.es_administrador = es_administrador
