30e01cfa3765bac790878beac60caa380b71f281
mariam130388 <mariam.tomas88@gmail.com>
2023-12-01 12:27:32 -0300

    se corrigen errores en la creacion de tablas

--- controladores/usuario_controlador.py
+++ controladores/usuario_controlador.py
@@ -1,11 +1,19 @@
-from app import db
+from flask import request, jsonify
+from app import app, db
 from modelos.usuario_modelo import *
 
-class Usuario(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.String(100))
-    email = db.Column(db.String(100))
+# Endpoint para obtener todos los usuarios
+@app.route('/usuarios', methods=['GET'])
+def obtener_usuarios():
+    usuarios = usuarios.query.all()
+    resultado = [{'id': usuario.id, 'nombre': usuario.nombre, 'email': usuario.email} for usuario in usuarios]
+    return jsonify(resultado), 200
 
-    def _init_(self, nombre, email):
-        self.nombre = nombre
-        self.email=email
+# Endpoint para agregar un nuevo usuario
+@app.route('/usuarios', methods=['POST'])
+def agregar_usuario():
+    data = request.json
+    nuevo_usuario = Usuario(nombre=data['nombre'], email=data['email'])
+    db.session.add(nuevo_usuario)
+    db.session.commit()
+    return jsonify({'mensaje': 'Usuario agregado correctamente'}),201
--- modelos/usuario_modelo.py
+++ modelos/usuario_modelo.py
@@ -1,19 +1,10 @@
-from flask import request, jsonify
-from app import app, db
-from usuario_modelo import Usuario
+from app import db
 
-# Endpoint para obtener todos los usuarios
-@app.route('/usuarios', methods=['GET'])
-def obtener_usuarios():
-    usuarios = Usuario.query.all()
-    resultado = [{'id': usuario.id, 'nombre': usuario.nombre, 'email': usuario.email} for usuario in usuarios]
-    return jsonify(resultado), 200
+class Usuario(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(100))
+    email = db.Column(db.String(100))
 
-# Endpoint para agregar un nuevo usuario
-@app.route('/usuarios', methods=['POST'])
-def agregar_usuario():
-    data = request.json
-    nuevo_usuario = Usuario(nombre=data['nombre'], email=data['email'])
-    db.session.add(nuevo_usuario)
-    db.session.commit()
-    return jsonify({'mensaje': 'Usuario agregado correctamente'}),201
+    def _init_(self, nombre, email):
+        self.nombre = nombre
+        self.email=email
