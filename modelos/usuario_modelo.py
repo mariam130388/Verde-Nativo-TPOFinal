44fcdf064e4f882776f496612b4ddcafa8e300ce
mariam130388 <mariam.tomas88@gmail.com>
2023-12-03 18:02:02 -0300

    se corrigen errores en definiciones de datos

--- modelos/usuario_modelo.py
+++ modelos/usuario_modelo.py
@@ -3,8 +3,8 @@ from app import db
 class Usuario(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     usuario = db.Column(db.String(100))
-    contraseña = db.Column(db.Integer(100))
-    es_administrador= db.Column(db.Integer(1))
+    contraseña = db.Column(db.String(8))
+    es_administrador= db.Column(db.Boolean)
 
     def _init_(self, usuario, contraseña, es_administrador):
         self.usuario = usuario
